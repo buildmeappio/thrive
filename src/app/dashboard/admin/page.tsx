d6c626aaef1973e7543c60d8ee9af9e1ddbbcea7
faiqhussainglitch <faiqhussain912@gmail.com>
2025-08-19 00:11:41 +0500

    Responsive Admin Pannel

--- src/app/dashboard/admin/page.tsx
+++ src/app/dashboard/admin/page.tsx
@@ -1,3 +1,25 @@
+// import {
+//   HomeStats,
+//   HomeCards,
+//   HomeDataTable,
+//   HomeRecentUpdates,
+// } from '@/shared/components/dashboard/admin/home';
+// import React from 'react';
+
+// export default function AdminDashboardPage() {
+//   return (
+//     <div className="py- flex gap-4 px-2">
+//       <div className="flex w-[70%] flex-col gap-6">
+//         <HomeCards />
+//         <HomeDataTable />
+//       </div>
+//       <div className="flex w-[30%] flex-col gap-6 mt-14">
+//         <HomeRecentUpdates />
+//         <HomeStats />
+//       </div>
+//     </div>
+//   );
+// }
 import {
   HomeStats,
   HomeCards,
@@ -8,12 +30,12 @@ import React from 'react';
 
 export default function AdminDashboardPage() {
   return (
-    <div className="py- flex gap-4 px-2">
-      <div className="flex w-[70%] flex-col gap-6">
+    <div className="py- flex gap-4 px-2 max-sm:flex-col max-sm:gap-6">
+      <div className="flex w-[70%] flex-col gap-6 max-sm:w-full">
         <HomeCards />
         <HomeDataTable />
       </div>
-      <div className="flex w-[30%] flex-col gap-6 mt-14">
+      <div className="flex w-[30%] flex-col gap-6 mt-14 max-sm:w-full max-sm:mt-0">
         <HomeRecentUpdates />
         <HomeStats />
       </div>
