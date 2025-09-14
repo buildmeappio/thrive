29b7408dcb387ae7653537fa51bf0a889fa14846
abdulbari149 <abdulbari131103@gmail.com>
2025-09-15 02:59:31 +0500

    feat: prefixed images on /admin

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   basePath: "/admin",
+  images: {
+    path: '/admin/_next/image'
+  }
 };
 
 export default nextConfig;
