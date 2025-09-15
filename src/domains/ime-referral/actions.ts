6de0a86c79524a8db99e35011ba7e14d80a85d39
abdulbari149 <abdulbari131103@gmail.com>
2025-09-15 18:56:50 +0500

    feat: make pages dynamic

--- src/domains/ime-referral/actions.ts
+++ src/domains/ime-referral/actions.ts
@@ -2,18 +2,39 @@
 
 import { type IMEFormData } from '@/store/useIMEReferralStore';
 import { imeReferralHandlers } from './server';
+import { getCurrentUser } from '../auth/server/session';
+import { redirect } from 'next/navigation';
+import { URLS } from '@/constants/routes';
 
 export const createIMEReferral = async (data: IMEFormData, isDraft: boolean) => {
+  const user = await getCurrentUser();
+
+  if (!user) {
+    redirect(URLS.LOGIN);
+  }
+
   const result = await imeReferralHandlers.createIMEReferral(data, isDraft);
   return result;
 };
 
 export const getReferrals = async () => {
+  const user = await getCurrentUser();
+
+  if (!user) {
+    redirect(URLS.LOGIN);
+  }
+
   const result = await imeReferralHandlers.getReferrals();
   return result;
 };
 
 export const getReferralDetails = async (referralId: string) => {
+  const user = await getCurrentUser();
+
+  if (!user) {
+    redirect(URLS.LOGIN);
+  }
+
   const result = await imeReferralHandlers.getReferralDetails(referralId);
   return result;
 };
