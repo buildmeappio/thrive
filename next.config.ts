74900c96bcba8731958c44ce964ee999280482e1
abdulbari149 <abdulbari131103@gmail.com>
2025-10-15 22:50:37 +0500

    feat: change to static remote pattern

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,13 @@
 import type { NextConfig } from 'next';
 
-const cdnUrl = process.env.NEXT_PUBLIC_CDN_URL || 'https://assets.thriveassessmentcare.com';
-const cdn = new URL(cdnUrl);
-const protocol: 'http' | 'https' = cdn.protocol === 'https:' ? 'https' : 'http';
-const hostname = cdn.hostname;
-
 const nextConfig: NextConfig = {
   /* config options here */
   basePath: '/organization',
   images: {
     remotePatterns: [
       {
-        protocol: protocol,
-        hostname: hostname,
+        protocol: 'https',
+        hostname: 'assets.thriveassessmentcare.com',
       },
     ],
   },
