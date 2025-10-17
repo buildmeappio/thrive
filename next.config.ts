a2b6aa1180aab65804cb4a5c5880b580c9183129
abdulbari149 <abdulbari131103@gmail.com>
2025-10-17 19:43:36 +0500

    feat: filter get languages

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,6 @@ const cdnUrl = "https://assets.thriveassessmentcare.com";
 const protocol = cdnUrl.startsWith("https") ? "https" : "http";
 const hostname = cdnUrl.split("//")[1];
 
-console.log({ protocol, hostname });
-
 const nextConfig: NextConfig = {
   /* config options here */
   basePath: "/examiner",
