ba1342ff36ad924b347923f5a18cb2147791f19a
Mahroz Abbas <mahrozabass@gmail.com>
2025-11-27 01:21:41 +0500

    fix: update favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,9 +6,6 @@ import { degular, poppins } from "@/styles/fonts";
 export const metadata: Metadata = {
   title: "Thrive IME Platform",
   description: "Independent Medical Examiner Platform",
-  icons: {
-    icon: '/images/favicon.ico',
-  },
 };
 
 export default function RootLayout({
@@ -20,12 +17,14 @@ export default function RootLayout({
     <html lang="en" className="light" style={{ colorScheme: "light" }}>
       <head>
         {/* Ensure proper mobile viewport to avoid auto zoom-out/scaling */}
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1"
+        />
       </head>
-      <body className={`${degular.variable} ${poppins.variable} font-degular antialiased`}>
-        <Provider>
-          {children}
-        </Provider>
+      <body
+        className={`${degular.variable} ${poppins.variable} font-degular antialiased`}>
+        <Provider>{children}</Provider>
       </body>
     </html>
   );
