a4725c1136f3135008600bfc29f1845e389b4406
Mahroz Abbas <mahrozabass@gmail.com>
2025-12-10 03:48:06 +0500

    update config files

--- prisma.config.ts
+++ prisma.config.ts
@@ -5,7 +5,7 @@ import "dotenv/config";
 export default defineConfig({
   schema: path.join("prisma"),
   datasource: {
-    url: process.env.DATABASE_URL,
+    url: process.env.DATABASE_URL!,
   },
   migrations: {
     path: path.join("src", "migrations"),
