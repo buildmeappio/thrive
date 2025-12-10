7501850c6820aeed46515c94d872ddc6a7246863
Mahroz Abbas <mahrozabass@gmail.com>
2025-12-10 07:45:31 +0500

    fix: deleted some duplicate files from the settings domain

--- src/domains/setting/components/OnboardingSteps/AvailabilityTabs/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as WeeklyHours } from "./weekly-hours";
-export { default as OverrideHours } from "./override-hours";
-export { default as BookingOptions } from "./booking-options";
