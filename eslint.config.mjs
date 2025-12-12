e0a2453689f80a3f122bffb78d307a29edb31312
abdulbari131103@gmail.com <abdulbari149>
2025-12-12 05:07:13 -0500

    fix: eslint issue and docker-compose getting changed on server

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -23,8 +23,10 @@ const eslintConfig = [
         },
       ],
     },
-    ignores: ["node_modules/", ".next/", "out/", "build/", "next-env.d.ts"],
   },
+  {
+    ignores: ["node_modules/", ".next/", "out/", "build/", "next-env.d.ts"],
+  }
 ];
 
 export default eslintConfig;
