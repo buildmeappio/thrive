4c6fc873fe71a76bb8a7440ad15757aca2d4ddf4
abdulbari131103@gmail.com <abdulbari149>
2025-12-15 18:57:53 +0500

    fix: aws region issue

--- src/constants/variables.ts
+++ src/constants/variables.ts
@@ -1,7 +1,7 @@
 export const ENV = {
   // Server Configuration
   DATABASE_URL: process.env.DATABASE_URL,
-  AWS_REGION: process.env.AWS_REGION,
+  AWS_REGION: process.env.AWS_S3_REGION,
   AWS_ACCESS_KEY_ID: process.env.AWS_S3_ACCESS_KEY_ID,
   AWS_SECRET_ACCESS_KEY: process.env.AWS_S3_SECRET_ACCESS_KEY,
   AWS_S3_BUCKET: process.env.AWS_S3_BUCKET_NAME,
