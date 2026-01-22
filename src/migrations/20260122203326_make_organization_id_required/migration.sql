f57bd1eeb45618d57727edc1f021e96f61479515
abdulbari131103@gmail.com <abdulbari149>
2026-01-23 01:48:54 +0500

    refactor: remove organization_id foreign key migration
    - Deleted migration script that made the `organization_id` field required in the `organization_roles` table.
    - This change simplifies the database schema by removing unnecessary foreign key constraints, enhancing maintainability.

--- src/migrations/20260122203326_make_organization_id_required/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "organization_roles" DROP CONSTRAINT "organization_roles_organization_id_fkey";
-
--- AddForeignKey
-ALTER TABLE "organization_roles" ADD CONSTRAINT "organization_roles_organization_id_fkey" FOREIGN KEY ("organization_id") REFERENCES "organizations"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
